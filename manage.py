c694737aa65babda0080fbd6a64bc14d8cb8d50a
Aaron Meier <aaron@0x41.ch>
2014-11-02 13:49:31 +0100

    Added license info

--- manage.py
+++ manage.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
+# Coffeenator: A simple web application to control coffee machines over network.
+# Copyright (C) 2014, Aaron Meier <aaron@bluespeed.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import os
 import sys
-
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webinterface.settings")
